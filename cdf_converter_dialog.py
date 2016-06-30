ebb2721929f9d7b6641ed5882f00efc342c28019
Ivan Busthomi <ivanbusthomi.github@gmail.com>
2016-06-30 17:06:32 +0700

    add crs description / name

--- cdf_converter_dialog.py
+++ cdf_converter_dialog.py
@@ -58,11 +58,17 @@ class CdfConverterDialog(QtGui.QDialog, FORM_CLASS):
 
 
     def on_select_crs_pressed(self):
+        """
+        define output crs
+        :return:
+        """
         crs_selector = QgsGenericProjectionSelector()
         crs_selector.show()
         crs_selector.exec_()
         self.authid = str(crs_selector.selectedAuthId())
-        self.output_crs.setText(self.authid)
+        selected_crs = QgsCoordinateReferenceSystem()
+        selected_crs.createFromString(self.authid)
+        self.output_crs.setText(selected_crs.description() + "    (" + self.authid+")")
 
     def on_browse_output_pressed(self):
         """
@@ -76,6 +82,10 @@ class CdfConverterDialog(QtGui.QDialog, FORM_CLASS):
             self.output_path.setText(output_file)
 
     def on_input_path_textChanged(self):
+        """
+        Check input path from browse dialog
+        :return:
+        """
         if not os.path.exists(self.input_path.text()):
             #self.display_log.append("Input file does not exist")
             print "Input file does not exist"
@@ -88,6 +98,10 @@ class CdfConverterDialog(QtGui.QDialog, FORM_CLASS):
             self.get_subdatasets()
 
     def get_subdatasets(self):
+        """
+        get subdataset
+        :return:
+        """
         self.select_subdataset.clear()
         netcdf = gdal.Open(self.file_path)
         list_subdatasets = []
@@ -98,10 +112,18 @@ class CdfConverterDialog(QtGui.QDialog, FORM_CLASS):
 
     @pyqtSlot(int)  # avoid currentIndexChanged signal to be emitted twice
     def on_select_subdataset_currentIndexChanged(self):
+        """
+        call get band
+        :return:
+        """
         self.subdataset = self.select_subdataset.currentText()
         self.get_bands()
 
     def get_bands(self):
+        """
+        Get band from subdataset
+        :return:
+        """
         #self.select_band.clear()
         netcdf_sd_path = 'NETCDF:"' + self.file_path + '":' + self.subdataset
         #self.display_log.append(netcdf_sd_path)
@@ -121,6 +143,10 @@ class CdfConverterDialog(QtGui.QDialog, FORM_CLASS):
 
     @pyqtSlot(int) # avoid currentIndexChanged signal to be emitted twice
     def on_select_band_currentIndexChanged(self):
+        """
+        update band and set it as output name
+        :return:
+        """
         self.band = str(self.select_band.currentIndex() + 1)
         self.input_title.setText(self.select_subdataset.currentText()+"_"+self.select_band.currentText())
         #self.display_log.clear()
@@ -144,9 +170,18 @@ class CdfConverterDialog(QtGui.QDialog, FORM_CLASS):
         subprocess.Popen(full_cmd, shell=True)
 
     def closeEvent(self, QCloseEvent):
+        """
+        handle close event
+        :param QCloseEvent:
+        :return:
+        """
         self.reject()
 
     def reject(self):
+        """
+        clear widget
+        :return:
+        """
         self.input_path.clear()
         self.output_path.clear()
         self.select_band.clear()
